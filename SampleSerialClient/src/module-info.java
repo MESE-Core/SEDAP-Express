47edcf94220316962ef4fb494c710a3ca7367c25
VolkerVoss <volker.v@gmx.net>
2024-04-12 12:29:04 +0200

    Update basic projects

---
+++ SampleSerialClient/src/module-info.java
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ * Copyright (C)2012-2023, German Federal Armed Forces - All rights reserved.
+ *
+ * MUKdo II
+ * Wibbelhofstraße 3
+ * 26384 Wilhelmshaven
+ * Germany
+ *
+ * This source code is part of the MEDAS/SEDAP Project.
+ * Person of contact (POC): Volker Voß, MUKdo II A, Wilhelmshaven
+ *
+ * Unauthorized use, modification, redistributing, copying, selling and
+ * printing of this file in source and binary form including accompanying
+ * materials is STRICTLY prohibited.
+ *
+ * This source code and it's parts is classified as OFFEN / NATO UNCLASSIFIED!
+ *******************************************************************************/
+/**
+ * 
+ */
+module SampleSerialClient {
+}
